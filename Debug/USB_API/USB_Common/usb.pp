61a88406ee64e694b357869a978388541e10bf54
Andrew Tesler <tesler.andrew@gmail.com>
2016-07-23 23:39:15 +0300

    tes

---
+++ Debug/USB_API/USB_Common/usb.pp
@@ -0,0 +1,125 @@
+# FIXED
+
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/usb.c
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/string.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/linkage.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h
+USB_API/USB_Common/usb.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdbool.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc10_a.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc12_a.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/aes.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/battbak.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/comp_b.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/crc.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dac12_a.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dma.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_spi.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_uart.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_spi.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/flashctl.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/gpio.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ldopwr.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/mpy32.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmap.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmm.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ram.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ref.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_a.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_b.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_c.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sd24_b.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sfr.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sysctl.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tec.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_a.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_b.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_d.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tlv.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ucs.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_spi.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_uart.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_i2c.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_spi.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/wdt_a.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_Common/device.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_Common/defMSP430USB.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_Common/usb.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_CDC_API/UsbCdc.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_PHDC_API/UsbPHDC.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_HID_API/UsbHidReq.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_MSC_API/UsbMscScsi.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/USB_config/descriptors.h
+USB_API/USB_Common/usb.obj: ../USB_API/USB_Common/../USB_Common/UsbIsr.h
+USB_API/USB_Common/usb.obj: C:/CodeComposer/watchdogMCU/USB_config/descriptors.h
+
+../USB_API/USB_Common/usb.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/string.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/linkage.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdbool.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc10_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc12_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/aes.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/battbak.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/comp_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/crc.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dac12_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dma.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_uart.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/flashctl.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/gpio.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ldopwr.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/mpy32.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmap.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmm.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ram.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ref.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sd24_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sfr.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sysctl.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tec.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_d.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tlv.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ucs.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_uart.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_i2c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/wdt_a.h: 
+../USB_API/USB_Common/../USB_Common/device.h: 
+../USB_API/USB_Common/../USB_Common/defMSP430USB.h: 
+../USB_API/USB_Common/../USB_Common/usb.h: 
+../USB_API/USB_Common/../USB_CDC_API/UsbCdc.h: 
+../USB_API/USB_Common/../USB_PHDC_API/UsbPHDC.h: 
+../USB_API/USB_Common/../USB_HID_API/UsbHidReq.h: 
+../USB_API/USB_Common/../USB_MSC_API/UsbMscScsi.h: 
+C:/CodeComposer/watchdogMCU/USB_config/descriptors.h: 
+../USB_API/USB_Common/../USB_Common/UsbIsr.h: 
+C:/CodeComposer/watchdogMCU/USB_config/descriptors.h: 
---
+++ Debug/hal.pp
@@ -0,0 +1,111 @@
+# FIXED
+
+hal.obj: ../hal.c
+hal.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+hal.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+hal.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+hal.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h
+hal.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h
+hal.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h
+hal.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdbool.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc10_a.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc12_a.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/aes.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/battbak.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/comp_b.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/crc.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dac12_a.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dma.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_spi.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_uart.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_spi.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/flashctl.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/gpio.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ldopwr.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/mpy32.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmap.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmm.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ram.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ref.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_a.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_b.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_c.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sd24_b.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sfr.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sysctl.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tec.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_a.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_b.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_d.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tlv.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ucs.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_spi.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_uart.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_i2c.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_spi.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/wdt_a.h
+hal.obj: ../USB_API/USB_Common/device.h
+hal.obj: ../USB_config/descriptors.h
+hal.obj: C:/CodeComposer/watchdogMCU/USB_API/USB_Common/usb.h
+hal.obj: ../hal.h
+hal.obj: C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h
+
+../hal.c: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_memmap.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/../deprecated/CCS/msp430f5xx_6xxgeneric.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdbool.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc10_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/adc12_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/inc/hw_regaccess.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/aes.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/battbak.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/comp_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/crc.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dac12_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/dma.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_a_uart.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_i2c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/eusci_b_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/flashctl.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/gpio.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ldopwr.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/mpy32.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmap.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/pmm.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ram.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ref.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/rtc_c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sd24_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sfr.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/sysctl.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tec.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_a.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_b.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/timer_d.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/tlv.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/ucs.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_a_uart.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_i2c.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/usci_b_spi.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/wdt_a.h: 
+../USB_API/USB_Common/device.h: 
+../USB_config/descriptors.h: 
+C:/CodeComposer/watchdogMCU/USB_API/USB_Common/usb.h: 
+../hal.h: 
+C:/CodeComposer/watchdogMCU/driverlib/MSP430F5xx_6xx/driverlib.h: 
